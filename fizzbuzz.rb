ce20796defa222b41a7f8db92410241d9d5f6e1b
Alexandra Fren <alexandra.fren@gmail.com>
2018-05-28 17:22:47 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,10 @@
 def fizzbuzz(number)
   if (number % 3 == 0 && number % 5 == 0)
     "Fizzbuzz"
-  end
   elsif number % 5 == 0
     "Buzz"
-  end
   elsif number % 3 == 0
     "Fizz"
-  end
   else
     "nil"
   end
